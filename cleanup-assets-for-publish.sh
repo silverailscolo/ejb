f1dd263f0b2b34bf496ff9366fdbee9306e3cf26
silverailscolo <dcc2@ebroerse.nl>
2026-01-31 22:03:14 +0000

    Deploying to gh-pages from @ silverailscolo/ejb@67090ce681e36741872635ffdf1477bdab939982 🚀

--- cleanup-assets-for-publish.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Remove these non-default (duplicate and unused) asset files - custom ejb, also in gh Deploy workflow
-rm -rf './_site/en-us/assets/downloads' './_site/en-us/assets/img' './_site/en-us/assets/pdf' './_site/en-us/assets/python'
